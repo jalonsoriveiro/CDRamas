6e60a0cb67475b7b242906358532daa2bc5d5d52
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-01-15 14:23:41 +0100

    C -Nueva clase

--- CDRamas.java
+++ CDRamas.java
@@ -15,7 +15,6 @@ public class CDRamas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo ");
-    }
-    
+        System.out.println("Hola Mundo ");        
+    }  
 }
